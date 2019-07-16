37bb80e18b849d619fafe32db7aaca7862df229e
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-07-16 14:53:44 +0800

    added templates

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return '<h1>Why so hard?</h1>'
+    return render_template('index.html')
 
 
 # @app.route("/another")
