1ea9b96cf5b7a0991e08c794afb8fa0529bf6dc9
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-07-16 17:42:39 +0800

    added non-functionong contact us form

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    signed_in = False
-    return render_template('index.html', signed_in=signed_in)
+    first_name = request.args.get('first_name')
+    return render_template('index.html', first_name=first_name)
 
 
 @app.route('/contact')
 def contact():
-    signed_in = False
-    return render_template('contact.html', signed_in=signed_in)
+    return render_template('contact.html')
 
 
 # @app.route("/<name>")
