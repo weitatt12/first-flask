5791719fec4c80eaf553276ab6fae28eb9285547
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-07-16 14:39:06 +0800

    added show route

--- server.py
+++ server.py
@@ -7,5 +7,14 @@ def index():
     return '<h1>Why so hard?</h1>'
 
 
+# @app.route("/another")
+# def show():
+#     return '<h1>Yo, Yo</h1>'
+
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username}"
+
+
 if __name__ == '__main__':
     app.run()
