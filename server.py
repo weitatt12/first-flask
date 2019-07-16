0b2bf0855bc827dc41f9d440b05688ec32639082
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-07-16 15:45:09 +0800

    added signgn in and sign out button

--- server.py
+++ server.py
@@ -4,17 +4,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    signed_in = False
+    return render_template('index.html', signed_in=signed_in)
+
+
+# @app.route("/<name>")
+# def index(name):
+#     name = name.upper()
+#     return render_template('index.html', name=name)
 
 
 # @app.route("/another")
 # def show():
 #     return '<h1>Yo, Yo</h1>'
 
-@app.route('/user/<username>')
+@app.route('/<username>')
 def show(username):
     return f"Hi {username}"
 
 
 if __name__ == '__main__':
     app.run()
+    # app.run(debug=True)
